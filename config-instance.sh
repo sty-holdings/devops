524355f4acd2da3ac8635233287b245476092efe
Scott Yacko <scott.yacko@sty-holdings.com>
2022-11-29 18:54:05 -0800

    Initial Check in

---
+++ config-instance.sh
@@ -0,0 +1,68 @@
+FILE=styh.profle
+if test -f "$FILE"; then
+	cat > styh.profile <<- EOF
+	echo
+	echo "========================="
+	echo "==  Updating locate DB =="
+	sudo updatedb
+	echo Done
+	echo "========================="
+	echo
+	echo "========================="
+	echo "Packages needing to be upgraded"
+	sudo apt list --upgradable
+	echo "=                       ="
+	echo " run the following command "
+	echo " to get updated list. "
+	echo " > sudo apt list --upgradable "
+	echo "========================="
+EOF
+fi
+
+FILE=.profle
+CONTAINS_STRING="# STYH startup profile"
+if ! grep -q CONTAINS_STRING "$FILE"; then
+	cat > .profile <<- EOF
+	echo #
+	echo # STYH startup profile
+	echo sh styh.profile
+	EOF
+fi
+
+FILE=.bash_aliases
+if test -f "$FILE"; then
+cat > .bash_aliases <<- EOF
+	#
+	# GOLANG
+	#
+	alias gopath='cd $GOPATH'
+	alias gobin='cd $GOBIN'
+	alias gobuild='cd $GOBUILD'
+	alias gosrc='cd $GOSRC'
+	alias goconfig='cd $GOCONFIG'
+	alias gopkg='cd $GOPKG'
+	alias goutils='cd $GOPATH/src/utils'
+
+	#
+	# BASH Shell
+	#
+	alias home='cd ~'
+	alias reset='source ~/.bashrc'
+	alias lsl='ls -la --color=auto'
+	alias back='cd ..'
+
+	#
+	# STYH Commands
+	#
+	alias styh='cd ~/styh/servers'
+	alias styhbin='cd ~/styh/servers/bin'
+	alias styhscripts='cd ~/styh/servers/scripts'
+	#
+	alias web='cd /var/www'
+	alias styhexternal='cd /var/www/styh_external_site.com'
+	alias styhwiki='cd /var/www/styh_wiki_site.com'
+	alias savupbeta='cd /var/www/savup_beta.com'
+	alias savupmy='cd /var/www/savup_my.com'
+	alias savupwaiting='cd /var/www/savup_waiting_list.com'
+	EOF
+fi
