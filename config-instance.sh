f6972db321d488302b790b71d3f3f14975027297
Scott Yacko <scott.yacko@sty-holdings.com>
2022-11-30 15:08:09 -0800

    Made additional updates for all systems

--- config-instance.sh
+++ config-instance.sh
@@ -1,77 +1,76 @@
+sudo apt update
 sudo apt-get install mlocate -y
+sudo apt-get install unzip
+sudo apt-get install zip
 
 
 FILE=.profile
-CONTAINS_STRING="# STYH startup profile"
-if ! grep -q CONTAINS_STRING "$FILE"; then
-  cp .profile .saved.profile
-	cat >> .profile <<- EOF
+ORIGINALFILE=.original.profile
+if [ -f "$HOME/$ORIGINALFILE" ]; then
+  cp "$HOME/$ORIGINALFILE" "$HOME/$FILE"
+else
+	cp "$HOME/$FILE" "$HOME/$ORIGINALFILE"
+fi
+
+
+cat >> "$HOME/$FILE" <<- EOF
 	
 	#
 	# STYH startup profile
-	sh styh.profile
-	EOF
-fi
 
-FILE=styh.profile
-cat > styh.profile <<- EOF
-#!/bin/bash
-if [ -f "$HOME/.bash_aliases" ]; then
-    . "$HOME/.bash_aliases"
-fi
-
-echo
-echo "========================="
-echo "==  Updating locate DB =="
-sudo updatedb
-echo Done
-echo "========================="
-echo
-echo
-echo "========================="
-echo "Packages needing to be upgraded"
-sudo apt list --upgradable
-echo "=                       ="
-echo " run the following command "
-echo " to get updated list. "
-echo " > sudo apt list --upgradable "
-echo "========================="
+	echo
+	echo "==> Updating locate DB =="
+	sudo updatedb
+	echo "==> Done"
+	echo
+	echo
+	echo "==> Packages needing to be upgraded"
+	sudo apt update
+	sudo apt list --upgradable
+	echo
+	echo
+	echo "==> Run the following command "
+	echo "==> to get updated list. "
+	echo "==> sudo apt list --upgradable "
+	echo
+	echo
 EOF
+
 
 FILE=.bash_aliases
-cat > .bash_aliases <<- EOF
-#
-# GOLANG
-#
-alias gopath='cd $GOPATH'
-alias gobin='cd $GOBIN'
-alias gobuild='cd $GOBUILD'
-alias gosrc='cd $GOSRC'
-alias goconfig='cd $GOCONFIG'
-alias gopkg='cd $GOPKG'
-alias goutils='cd $GOPATH/src/utils'
+cat > "$HOME/$FILE" <<- EOF
+	#
+	# GOLANG
+	#
+	alias gopath='cd $GOPATH'
+	alias gobin='cd $GOBIN'
+	alias gobuild='cd $GOBUILD'
+	alias gosrc='cd $GOSRC'
+	alias goconfig='cd $GOCONFIG'
+	alias gopkg='cd $GOPKG'
+	alias goutils='cd $GOPATH/src/utils'
 
-#
-# BASH Shell
-#
-alias home='cd ~'
-alias reset='source ~/.bashrc'
-alias lsl='ls -la --color=auto'
-alias back='cd ..'
+	#
+	# BASH Shell
+	#
+	alias home='cd ~'
+	alias reset='source ~/.bashrc'
+	alias lsl='ls -la --color=auto'
+	alias back='cd ..'
 
-#
-# STYH Commands
-#
-alias styh='cd ~/styh/servers'
-alias styhbin='cd ~/styh/servers/bin'
-alias styhscripts='cd ~/styh/servers/scripts'
-#
-alias web='cd /var/www'
-alias styhexternal='cd /var/www/styh_external_site.com'
-alias styhwiki='cd /var/www/styh_wiki_site.com'
-alias savupbeta='cd /var/www/savup_beta.com'
-alias savupmy='cd /var/www/savup_my.com'
-alias savupwaiting='cd /var/www/savup_waiting_list.com'
+	#
+	# STYH Commands
+	#
+	alias styh='cd ~/styh/servers'
+	alias styhbin='cd ~/styh/servers/bin'
+	alias styhscripts='cd ~/styh/servers/scripts'
+	#
+	alias web='cd /var/www'
+	alias styhexternal='cd /var/www/styh_external_site.com'
+	alias styhwiki='cd /var/www/styh_wiki_site.com'
+	alias savupbeta='cd /var/www/savup_beta.com'
+	alias savupmy='cd /var/www/savup_my.com'
+	alias savupwaiting='cd /var/www/savup_waiting_list.com'
 EOF
 
-. ~/.profile
+. ~/.bashrc
