1c19f8636c3864eac52f97f129bc9f558be83e33
Scott Yacko <scott.yacko@sty-holdings.com>
2022-12-09 10:30:45 -0800

    Added open internet listen to nats conf file

--- NATS-1.4-create-config-file.sh
+++ NATS-1.4-create-config-file.sh
@@ -6,7 +6,8 @@
 echo "==> Creating nats-server configuration file"
 cat > "$MY_NATS_HOME/nats.conf" <<- EOF
 
-	listen: localhost:4222
+	#listen: localhost:4222 # This will only allow access on the local host machine
+	listen: 0.0.0.0:4222 # This will allow access from anywhere on the internet
 
 	include 'includes/$MY_NATS_RESOLVER' # Pull in from file
 
