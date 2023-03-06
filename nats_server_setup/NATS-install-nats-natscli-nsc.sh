feb66b615474a7b2beafaf9271118ceaba548c00
Scott Yacko <scott.yacko@sty-holdings.com>
2023-03-05 19:00:23 -0800

    Refactor NATS Tools installation

--- nats_server_setup/NATS-install-nats-natscli-nsc.sh
+++ nats_server_setup/NATS-install-nats-natscli-nsc.sh
@@ -1,81 +1,57 @@
 #!/bin/bash -vx
 #
-# This will install NATS, NATS CLI, and NSC
+# This will install NATS, NATSCLI, and NSC
 #
+NATS_HOME=$1
+NATS_BIN=$2
+NATSCLI_BIN=$3
+NSC_BIN=$4
 
-# TODO Make this optional
-if grep -q $MY_NATS_HOME "/etc/fstab"; then
-	echo "==> NATS home is already mounted"
+echo " - Setting up NATS user, group, home directory, and permissions"
+if [ -f "$NATS_HOME"/includes ]; then
+  echo "   - $NATS_HOME/includes already exists. No action taken."
 else
-	echo "==> Mounting NATS home"
-	sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-	sudo mkdir -p $MY_NATS_HOME
-	sudo mount -o discard,defaults /dev/sdb $MY_NATS_HOME
-	sudo chmod a+w $MY_NATS_HOME
-	SHOW_MOUNT_NOTES=1
+  mkdir -p "$NATS_HOME"/includes
 fi
 
-echo "--------------"
-echo "==> Setting up NATS user, group, home directory, and permissions"
-mkdir -p $MY_NATS_HOME/includes
-mkdir -p $MY_NATS_HOME/jwt
-sudo useradd --home /mnt/disks/nats_home/ -M -s /bin/false -g nats -G google-sudoers nats
-sudo groupadd nats
-sudo chgrp -R nats "$MY_NATS_HOME"
-sudo chmod g+s "$MY_NATS_HOME"
- 
-
-echo "--------------"
-echo "==> Setting NATS user and group"
-sudo chgrp -R nats "$MY_NATS_HOME"
-sudo chmod g+s "$MY_NATS_HOME"
-sudo useradd --home /mnt/disks/nats_home/ -M -s /bin/false -g nats -G google-sudoers nats
-
-cd $HOME
-echo
-echo
-if [ -d "$HOME/nats" ]; then
-	echo "==> SKIPPING - Creating link to $MY_NATS_HOME - ALREADY EXISTS"
-	echo
-	echo
+if [ -f "$NATS_HOME"/includes ]; then
+  echo "   - $NATS_HOME/jwt already exists. No action taken."
 else
-	echo "==> Creating link to $MY_NATS_HOME called nats in $HOME"
-	sudo ln -s $MY_NATS_HOME $HOME/nats 
-	echo
-	echo
+  mkdir -p "$NATS_HOME"/jwt
 fi
 
-
-# NATS Server
-cd $MY_NATS_HOME
-echo "--------------"
-echo "==> Installing NATS Server"
-if [ -f "$MY_NATS_BIN" ]; then
-	echo "Server has already been installed"
+if grep -q "nats" "/etc/passwd"; then
+  echo "   - NATS user already exist. No action taken."
 else
-	curl -L https://github.com/nats-io/nats-server/releases/download/v2.9.8/nats-server-v2.9.8-linux-386.zip -o nats-server-v2.9.8.zip
+  sudo useradd --home /mnt/disks/nats_home/ -M -s /bin/false -g nats -G google-sudoers nats
+  sudo groupadd nats
+  sudo chgrp -R nats "$NATS_HOME"
+  sudo chmod g+s "$NATS_HOME"
+fi
+
+echo " - Installing NATS server at $NATS_BIN"
+if [ -f "$NATS_BIN" ]; then
+	echo "   - Server has already been installed"
+else
+	curl -L https://github.com/nats-io/nats-server/releases/download/v2.9.8/nats-server-v2.9.15-linux-386.zip -o nats-server-v2.9.15.zip
 	unzip nats-server-v2.9.8.zip -d nats-server
-	sudo cp "$MY_NATS_HOME/nats-server/nats-server-v2.9.8-linux-386/nats-server" $MY_NATS_BIN
+	sudo cp "$NATS_HOME/nats-server/nats-server-v2.9.8-linux-386/nats-server" $NATS_BIN
 fi
 
-# NATS CLI
-if [ -f "$MY_NATS_CLI_BIN" ]; then
-	echo "CLI has already been installed"
+echo " - Installing NATSCLI server at $NATSCLI_BIN"
+if [ -f "$NATSCLI_BIN" ]; then
+	echo "   - NATSCLI has already been installed"
 else
-	echo "--------------"
-	echo "==> Installing NATS CLI"
 	curl -L https://github.com/nats-io/natscli/releases/download/v0.0.35/nats-0.0.35-linux-386.zip -o nats-cli-v0.0.35.zip
 	unzip nats-cli-v0.0.35.zip -d nats-cli
-	sudo cp "$MY_NATS_HOME/nats-cli/nats-0.0.35-linux-386/nats" $MY_NATS_CLI_BIN
+	sudo cp "$NATS_HOME/nats-cli/nats-0.0.35-linux-386/nats" "$NATSCLI_BIN"
 fi
 
-# NATS NCS
-if [ -f "$MY_NSC_BIN" ]; then
-	echo "NSC has already been installed"
+echo " - Installing NSC server at $NSC_BIN"
+if [ -f "$NSC_BIN" ]; then
+	echo "   - NSC has already been installed"
 else
-	echo "--------------"
-	echo "==> Installing NSC"
-	curl -L https://github.com/nats-io/nsc/releases/download/2.7.4/nsc-linux-386.zip -o nsc.7.4.zip
+	curl -L https://github.com/nats-io/nsc/releases/download/2.7.8/nsc-linux-386.zip -o nsc.7.8.zip
 	unzip nsc.7.4.zip -d nsc
-	sudo cp "$MY_NATS_HOME/nsc/nsc" $MY_NSC_BIN
+	sudo cp "$NATS_HOME/nsc/nsc" $NSC_BIN
 fi
