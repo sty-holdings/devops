574a8716e09578552122f7fc7c356c206a1e8a1a
Scott Yacko <scott.yacko@sty-holdings.com>
2023-03-08 19:09:14 -0800

    Removed testing echo’s

--- shared/execute-command.sh
+++ shared/execute-command.sh
@@ -5,11 +5,9 @@
 
 # shellcheck disable=SC2129
 function executeCommand() {
-  echo "PASSED A"
   echo "$1" >>"$NATS_HOME"/NATS_log_file 2>> "$NATS_HOME"/NATS_log_file
   ${1} >> "$NATS_HOME"/NATS_log_file 2>> "$NATS_HOME"/NATS_log_file
   echo >> "$NATS_HOME"/NATS_log_file 2>> "$NATS_HOME"/NATS_log_file
-  echo "PASSED b"
 }
 
 
